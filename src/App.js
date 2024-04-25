390fe67e6cd1e6b05ded485411acc02fda7e3d16
im-siva-kona <143621207+im-siva-kona@users.noreply.github.com>
2024-04-25 21:50:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from 'react';
+
+import Services from './comps/Services';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold text-center mb-8">Service Booking App</h1>
       </header>
+      <Services />
     </div>
   );
 }
